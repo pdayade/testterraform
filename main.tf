095f66291d180436328baed56fa7e7e6e2f63fce
Pawan <dyadepawan1995@gmail.com>
2025-01-11 16:17:26 +0530

    My first commit

---
+++ main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+    region = "us-east-1"
+  
+}
+resource "aws_vpc" "main" {
+  cidr_block = "20.20.0.0/16"
+  tags = {
+    Name = "main"
+  }
+}
+resource "aws_subnet" "1subnet" {
+    vpc_id = aws_vpc.Myvpc.vpc_id
+    cidr_block = "20.20.1.0/24"
+
+    tags = {
+        Name = "Public"  
+    }
+}
+resource "aws_subnet" "2subnet" {
+    vpc_id = aws_vpc.Myvpc.vpc_id
+    cidr_block = "20.20.2.0/24"
+
+    tags = {
+        Name = "Private"  
+    }
+}
+
+resource "aws_instance" "firstec2" {
+    ami = data.aws_ami.myami.vpc_id
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.main.id
+}
+data "aws_ami" "myami" {
+    owners = ["amazon"]
+    most_recent = true
+
+    filter {
+        name = "name"
+        values = ["amzn2-ami-kernal-5.10**"]
+        }
+}
